18e2adf96effe11305535e82cdefc797f5c2c738
Shanu <prathms007@gmail.com>
2023-06-09 23:24:36 +0530

    main: Register and run grpc server.

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/shanukun/cash/cash_proto"
+	service "github.com/shanukun/cash/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	address string
+	expire  int
+	cleanup int
+)
+
+func parseFlags() {
+	flag.StringVar(&address, "addr", ":8001", "address")
+	flag.IntVar(&expire, "exp", 7, "expiration (min)")
+	flag.IntVar(&cleanup, "clu", 3, "cleanup after expiration (min)")
+	flag.Parse()
+}
+
+func main() {
+	parseFlags()
+
+	opts := []grpc.ServerOption{
+		grpc.MaxConcurrentStreams(100),
+	}
+
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterCacheServiceServer(
+		grpcServer,
+		service.NewCacheService(time.Duration(expire)*time.Minute,
+			time.Duration(cleanup)*time.Minute))
+
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("start error %v", err)
+	}
+    fmt.Println("server running on:", address)
+	if err := grpcServer.Serve(lis); err != nil {
+        log.Fatalf("grpc server failed: %v\n", err)
+	}
+}
