77b2a193680f9b8c7abaeff4d0a96ba3faf59b73
Shanu <prathms007@gmail.com>
2023-07-04 16:36:13 +0530

    demo: Clean demo so it may be referred to.

--- demo/demo.go
+++ demo/demo.go
@@ -24,31 +24,34 @@ func main() {
 	// Get address from flag
 	flag.StringVar(&address, "addr", "127.0.0.1:8001", "Setress on which you want to run server")
 	flag.Parse()
+
+    // Connecting to server
 	conn, err = grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %s", err)
 	}
 	defer conn.Close()
 
+    // Creating new client
 	c := pb.NewCacheServiceClient(conn)
 	ctx := context.Background()
 
 	// Set key
 	keyVal1 := &pb.String{
-		Key:        "22",
-		Value:      "knrt10",
+		Key:        "author",
+		Value:      "Brandon Sanderson",
 		Expiration: "-1m",
 	}
 
 	keyVal2 := &pb.String{
-		Key:        "distributed",
-		Value:      "systems",
+		Key:        "book",
+		Value:      "Mistborn",
 		Expiration: "20s",
 	}
 
 	keyVal3 := &pb.String{
-		Key:        "24",
-		Value:      "Palash",
+		Key:        "planet",
+		Value:      "Roshar",
 		Expiration: "2min10s",
 	}
 
@@ -72,10 +75,17 @@ func main() {
 
 	// Get key
 	keyGet := &pb.Key{
-		Key: "distributed",
+		Key: "book",
 	}
 
-	// list funcs
+	getKeyRes, err := c.Get(ctx, keyGet)
+	if err != nil {
+		log.Fatalf("Error when calling Get: Key: %s %s", keyGet.Key, err)
+	}
+	fmt.Println("Response from server for getting a key", getKeyRes)
+
+
+	// String item for List
 	keyVal4 := &pb.String{
 		Key:        "tbr",
 		Value:      "mistborn",
@@ -88,24 +98,21 @@ func main() {
 		Expiration: "2min10s",
 	}
 
+    // Left push
 	c.LPush(ctx, keyVal4)
+    // Right push
 	c.RPush(ctx, keyVal5)
 
 	keyGetList := &pb.Key{
 		Key: "tbr",
 	}
 
+    // Get complete list
 	list, err := c.GetList(ctx, keyGetList)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(list.List)
-
-	getKeyRes, err := c.Get(ctx, keyGet)
-	if err != nil {
-		log.Fatalf("Error when calling Get: Key: %s %s", keyGet.Key, err)
-	}
-	fmt.Println("Response from server for getting a key", getKeyRes)
 
 	// Delete Key
 	deleteKeyRes, err := c.DeleteKey(ctx, keyGet)
@@ -114,6 +121,7 @@ func main() {
 	}
 	fmt.Println("Response from server after deleting a key", deleteKeyRes)
 
+    // Set Key, Field and Value for HashMap
 	keyVal6 := &pb.HashMapItem{
 		Key:        "mybooks",
 		Field:      "fantasy",
@@ -128,6 +136,7 @@ func main() {
 		Expiration: "2min10s",
 	}
 
+    // Set Hash Map values
 	c.HMSet(ctx, keyVal6)
 	c.HMSet(ctx, keyVal7)
 
@@ -135,12 +144,14 @@ func main() {
 		Key: "mybooks",
 	}
 
+    // Get HashMap as a List
 	hmlist, err := c.GetHashMap(ctx, keyGetHashMap)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(hmlist.List)
 
-	c.DeleteAll(ctx, &empty.Empty{})
 
+    // Deleting all keys
+	c.DeleteAll(ctx, &empty.Empty{})
 }
